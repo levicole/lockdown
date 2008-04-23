08cfc41c18799c94c9e2161ee6264e196224649b
Andrew Stone <andy@stonean.com>
2008-04-23 10:52:58 -0400

    lockdown install working better with rubigen

--- app_generators/lockdown/lockdown_generator.rb
+++ app_generators/lockdown/lockdown_generator.rb
@@ -1,29 +1,24 @@
 class LockdownGenerator < RubiGen::Base
-  LIB_DIR = "lib/lockdown"
-  GEM_DIR =  File.join File.expand_path(File.dirname(__FILE__)), ".."
+  
+  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
+                              Config::CONFIG['ruby_install_name'])
+  
+  default_options :author => "Andrew Stone"
+  
+  attr_reader :name
+  
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @destination_root = File.expand_path(args.shift)
+    @name = base_name
+  end
 
   def manifest
     record do |m|
-      m.directory(LIB_DIR)
-
-      src_access_file = File.join(GEM_DIR, "templates", "access.rb")
-      dest_access_file = File.join(LIB_DIR, "access.rb")
-      
-      m.file(src_access_file, dest_access_file)
-      
-      puts <<-MSG
-      ... Copied access.rb template to lib/lockdown
-      MSG
-      
-      src_session_file = File.join(GEM_DIR, "templates", "session.rb")
-      dest_session_file = File.join(LIB_DIR, "session.rb")
-      
-      m.file(src_session_file, dest_session_file)
-      
-      puts <<-MSG
-      ... Copied sesion.rb template to lib/lockdown
-      MSG
+      m.directory "lib/lockdown"
+      m.file "session.rb", "lib/lockdown/session.rb"
+      m.file "access.rb", "lib/lockdown/access.rb"
     end
   end
 end
-
